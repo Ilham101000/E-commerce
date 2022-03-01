6ffad6a8820d5e0ec0fa67075b3a0d3bee034156
Ilham <ilhamuntung123@gmail.com>
2022-03-01 10:58:40 +0700

    memperbarui halaman daftar

--- daftar.php
+++ daftar.php
@@ -4,15 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="daftar.css">
     <title>Daftar</title>
 </head>
 <body>
     <div class="container">
         <form action="" method="post">
-            <tr>
-                <td><label for="nama">Masukan Nama Lengkap :</label></td>
-                <td><input type="text" name="nama" id="nama"></td>
-            </tr>
+            
+                <label for="nama">Masukan Nama Lengkap</label>
+                <input type="text" name="nama" id="nama">
+            
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email">        
+            
+                <label for="telp">No.Handphone</label>
+                <input type="text" name="telp" id="telp">
+            
+                <label for="pass1">Password</label>
+                <input type="password" name="pass1" id="pass1">
+            
+                <label for="pass2">Konfirmasi Password</label>
+                <input type="password" name="pass2" id="pass2"><br>
+        
+                <button type="submit" name="daftar" class="daftar">Daftar</button>
+            </table>
         </form>
     </div>
 </body>
